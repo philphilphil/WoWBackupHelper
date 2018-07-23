1bd5028bf744588aa604069b639921b2832d5c84
Phil Baum <phil@phib.io>
2018-07-23 11:55:46 +0200

    Added some ui

---
+++ WoWBackupHelper/FileSystemHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoWBackupHelper
+{
+    class FileSystemHelper
+    {
+
+        public FileSystemHelper()
+        {
+
+        }
+
+        public void GetWoWFolderPath()
+        {
+            //Directory wowDir = new Directory();
+            if(Directory.Exists(@"C:\Program Files (x86)\World of Warcraft"))
+            {
+
+            }
+        }
+    }
+}
