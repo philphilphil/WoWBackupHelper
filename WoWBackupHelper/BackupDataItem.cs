68d0fced5deb02c9bf88e62be412948568ee4cb8
Phil Baum <phil@phib.io>
2018-08-09 12:20:29 +0200

    Grid changes and backup types

---
+++ WoWBackupHelper/BackupDataItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoWBackupHelper
+{
+    class BackupDataItem
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+
+    }
+}
+
--- WoWBackupHelper/Form1.cs
+++ WoWBackupHelper/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -25,19 +26,42 @@ namespace WoWBackupHelper
         private void FillGridWithBackups()
         {
             dataGridView1.Rows.Clear();
-            Regex reg = new Regex(@"WoW_UI-Addon_Backup_.*");
 
-            var files = Directory.GetFiles(@"C:\temp\backuptest\", "*.zip")
-                                 .Where(path => reg.IsMatch(path))
-                                 .ToList();
+            List<BackupDataItem> backupFiles = GetBackupFiles(@"C:\temp\backuptest\");
+
             int row = 0;
-            foreach (var item in files)
+            foreach (var item in backupFiles)
             {
                 dataGridView1.Rows.Add(1);
-                dataGridView1.Rows[row].Cells[0].Value = item;
+                dataGridView1.Rows[row].Cells[0].Value = item.Type;
+                dataGridView1.Rows[row].Cells[1].Value = item.Date;
                 row++;
             }
 
+        }
+
+        private List<BackupDataItem> GetBackupFiles(string pathToBackups)
+        {
+            string regexPattern = @"WoW_UI-(Full|Addon|Settings)-Backup_(.*).zip";
+            List<BackupDataItem> backupItemList = new List<BackupDataItem>();
+            Regex reg = new Regex(regexPattern);
+
+            var files = Directory.GetFiles(pathToBackups, "*.zip")
+                                 .Where(path => reg.IsMatch(path))
+                                 .ToList();
+
+            foreach (var item in files)
+            {
+                FileInfo f = new FileInfo(item);
+                Match matches = reg.Match(item);
+
+                if (matches.Success)
+                {
+                    var date = DateTime.ParseExact(matches.Groups[2].Value, "yyyyMMdd_HHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None);
+                    backupItemList.Add(new BackupDataItem { Type = matches.Groups[1].Value, Date = date });
+                }
+            }
+            return backupItemList;
         }
 
         private void btnStartBackup_Click(object sender, EventArgs e)
@@ -65,20 +89,31 @@ namespace WoWBackupHelper
             string wowPath = @"C:\Program Files (x86)\World of Warcraft\";
             string wtfFolder = wowPath + "WTF";
             string interfaceFolder = wowPath + "Interface";
-
-            var backupFileName = String.Format("WoW_UI-Addon_Backup_{0}.zip", DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+            string backypType = "";
 
             using (ZipFile zip = new ZipFile())
             {
 
                 zip.CompressionLevel = Ionic.Zlib.CompressionLevel.Default;
 
-                if (cbBackupWTF.Checked)
+                if (cbBackupWTF.Checked && cbBackupInterface.Checked)
+                {
+                    backypType = "Full";
                     zip.AddDirectory(wtfFolder, "WTF");
-
-                if (cbBackupInterface.Checked)
                     zip.AddDirectory(interfaceFolder, "Interface");
+                }
+                else if (cbBackupWTF.Checked)
+                {
+                    backypType = "Settings";
+                    zip.AddDirectory(wtfFolder, "WTF");
+                }
+                else if (cbBackupInterface.Checked)
+                {
+                    backypType = "Addons";
+                    zip.AddDirectory(interfaceFolder, "Interface");
+                }
 
+                var backupFileName = String.Format("WoW_UI-{0}-Backup_{1}.zip", backypType, DateTime.Now.ToString("yyyyMMdd_HHmmss"));
                 zip.SaveProgress += this.SetProgress;
                 zip.Save(@"C:\temp\backuptest\" + backupFileName);
             }
